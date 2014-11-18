5a419985d9c71babaa1d7cd6f2c279ab555b0cee
Strambauer <georg_daheim@yahoo.de>
2014-11-19 00:16:17 +0100

    First Button with Look and Feel for Android Wear
    Changed the Hello World Porgamm, so that a first look and feel for
    android wear is avaliable on square devices.

--- AndroidWear/LandmarkSet/wear/src/main/java/de/uni_freiburg/tf/landmarkset/MainActivity.java
+++ AndroidWear/LandmarkSet/wear/src/main/java/de/uni_freiburg/tf/landmarkset/MainActivity.java
@@ -2,7 +2,10 @@ package de.uni_freiburg.tf.landmarkset;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.wearable.view.CardScrollView;
 import android.support.wearable.view.WatchViewStub;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -13,12 +16,22 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+                //mTextView = (TextView) stub.findViewById(R.id.text);
+
+                //CardScrollView cardScrollView = (CardScrollView) findViewById(R.id.card_scroll_view);
+                //cardScrollView.setCardGravity(Gravity.BOTTOM);
             }
         });
+
+
+    }
+
+    public void onClick (View view){
+
     }
 }
