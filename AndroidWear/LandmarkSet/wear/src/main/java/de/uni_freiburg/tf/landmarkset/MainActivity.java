60d23bdc7da6dc23b46d5f553f285a9ba8f19345
Strambauer <georg_daheim@yahoo.de>
2014-11-20 23:25:04 +0100

    First experience with location service
    First experience with the functions needed to get the location on the
    wearable. Safety functions are implementet not yet.

--- AndroidWear/LandmarkSet/wear/src/main/java/de/uni_freiburg/tf/landmarkset/MainActivity.java
+++ AndroidWear/LandmarkSet/wear/src/main/java/de/uni_freiburg/tf/landmarkset/MainActivity.java
@@ -1,16 +1,110 @@
 package de.uni_freiburg.tf.landmarkset;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.wearable.view.CardScrollView;
 import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+//import com.google.android.gms.common.api.Result;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.FusedLocationProviderApi;
+import com.google.android.gms.wearable.Wearable;
+
+public class MainActivity extends Activity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     private TextView mTextView;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationClient mLocationClient;
+    private LocationRequest locationRequest;
+    private boolean connected = false;
+
+    private static final String TAG = "MyWearActivity";
+
+    private boolean servicesConnected() {
+        // Check that Google Play services is available
+        int resultCode =
+                GooglePlayServicesUtil.
+                        isGooglePlayServicesAvailable(this);
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode) {
+            // In debug mode, log the status
+            Log.d("Location Updates",
+                    "Google Play services is available.");
+            // Continue
+            return true;
+            // Google Play services was not available for some reason.
+            // resultCode holds the error code.
+        }else {
+            return false;
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location){
+
+        // Display the latitude and longitude in the UI
+        Log.e(TAG,"Latitude:  " + String.valueOf( location.getLatitude()) +
+                "\nLongitude:  " + String.valueOf( location.getLongitude()));
+    }
+
+    public void onConnected(Bundle dataBundle){
+        //Log.e("MyWearActivity", "Connected");
+        Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+        connected = true;
+
+        locationRequest = LocationRequest.create();
+
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        locationRequest.setInterval(2);
+
+        locationRequest.setFastestInterval(2);
+
+        locationRequest.setSmallestDisplacement(2);
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, locationRequest, this);
+
+
+
+    }
+    /*
+    public void onDisconnected(){
+        Log.e("MyWearActivity", "Disconnedted");
+        Toast.makeText(this, "Disconnected", Toast.LENGTH_SHORT).show();
+    }
+    */
+
+
+
+    public void onConnectionFailed(ConnectionResult connectionResult){
+        Log.e(TAG, "Connection Faild");
+        connected = false;
+    }
+
+    public void onConnectionSuspended(int cause){
+        Log.e(TAG, "Connection Suspended");
+        connected = false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +122,55 @@ public class MainActivity extends Activity {
             }
         });
 
+        //Log.e("MyWearActivity", "Play Service available:" + servicesConnected());
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        //mLocationClient = new LocationClient(this, this, this);
     }
 
-    public void onClick (View view){
+    protected void onResume(){
+        super.onResume();
+        Log.e(TAG, "OnResume");
+        mGoogleApiClient.connect();
+    }
+
+    protected void onPause(){
+        super.onPause();
+       mGoogleApiClient.disconnect();
+    }
+
+    protected void onStart(){
+        super.onStart();
+        //mLocationClient.connect();
+    }
+
+    protected void onStop(){
+        super.onStop();
+        //mLocationClient.disconnect();
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        Log.e(TAG, "Activity Result:" + resultCode);
+    }
+
+
+
+    public void showToast (View view){
+        Log.e(TAG, "Save Position Button is pressed");
+        Toast.makeText(this, "Button Pressed", Toast.LENGTH_SHORT).show();
+
+        Location location = LocationServices.FusedLocationApi
+                .getLastLocation(mGoogleApiClient);
+        Log.e("MyWearActivity", "\nGot location" +
+                "\nLatitude:  " + String.valueOf( location.getLatitude()) +
+                "\nLongitude:  " + String.valueOf( location.getLongitude()));
+
 
     }
 }
